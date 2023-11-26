6be70164780cfb428782b5b97b0bc7363555b955
ArantesVini <vinicius.daniel02@gmail.com>
2023-11-26 08:08:59 -0500

    loops in bash

---
+++ 9-Bash Scripting/loops.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# seq 1 10
+# sleep 1
+# seq 10 -1 1
+# sleep 1
+
+# for i in $(seq 1 10); do
+#     echo $i
+#     sleep 1
+# done
+
+while true; do 
+    echo "Eu tenho dois cachorros"
+    echo "Pet e Repet, o pet morreu"
+    echo "Qual cachorro sobrou?"
+    echo "Repet"
+    sleep 1
+done
