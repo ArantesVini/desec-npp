8e459484f5da95975bf6337c35f0ef82fd42099b
Vinicius Arantes <67487618+ArantesVini@users.noreply.github.com>
2023-11-29 08:54:21 -0300

    change the find_host script

--- 9-Bash Scripting/find_host.sh
+++ 9-Bash Scripting/find_host.sh
@@ -11,26 +11,15 @@ for host in $(seq 1 254); do
     second_port="37"
     third_port="30000"
     fourth_port="3000"
+    activate_port="1337"
     
     hping3 -c 1 -S -p $first_port $target_ping -q 2>/dev/null &
     hping3 -c 1 -S -p $second_port $target_ping -q 2>/dev/null &
     hping3 -c 1 -S -p $third_port $target_ping -q 2>/dev/null &
     hping3 -c 1 -S -p $fourth_port $target_ping -q 2>/dev/null &
+    hping3 -c 1 -S -p $activate_port $target_ping -q 2>/dev/null &
     
     if (( $(jobs -r -p | wc -l) >= max_processes )); then
-        wait -n
-    fi
-done
-
-wait
-
-new_max_processes=40
-
-for host in $(seq 1 254); do
-    target_connection="172.16.1."$host
-    nc -z -v -w 1 $target_connection 1337 2>/dev/null
-    
-    if (( $(jobs -r -p | wc -l) >= new_max_processes )); then
         wait -n
     fi
 done
