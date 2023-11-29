131e913638567616ad880a9fb51996bc9c5f1fb0
Vinicius Arantes <67487618+ArantesVini@users.noreply.github.com>
2023-11-29 12:42:36 -0300

    Update find_host.sh

--- 9-Bash Scripting/find_host.sh
+++ 9-Bash Scripting/find_host.sh
@@ -32,3 +32,19 @@ for host in $(seq 1 254); do
 done
 
 wait
+
+# Copilot suggest the following: Try it at home
+
+#!/bin/bash
+
+# for i in {1..254}; do
+#   ip="172.16.1.$i"
+#   ping -c 1 -W 1 $ip > /dev/null 2>&1
+#   if [ $? -eq 0 ]; then
+#     echo "Host $ip is up, checking port 1337"
+#     nc -zv $ip 1337 > /dev/null 2>&1
+#     if [ $? -eq 0 ]; then
+#       echo "Port 1337 is open on $ip"
+#     fi
+#   fi
+# done
